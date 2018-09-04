49cf79eb49f3ac36841fb0aaa740868d980bf338
Sickfly <mateusvasco3@gmail.com>
2018-09-04 07:35:00 -0300

    Alterações BD

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,38 +1,42 @@
 CREATE TABLE usuario (
-	id_usuario INT IDENTITY(1,1) NOT NULL,
+	id INT IDENTITY(1,1) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	senha VARCHAR(12) NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	nick VARCHAR(16) NOT NULL
-	PRIMARY KEY(id_usuario)
+	PRIMARY KEY(id)
 	);
 
 CREATE TABLE professor (
-	id_prof INT NOT NULL,
-	matricula VARCHAR(10) NOT NULL
-	PRIMARY KEY(id_prof)
+	id INT NOT NULL,
+	matricula VARCHAR(10) NOT NULL,
+	PRIMARY KEY(id)
 	);
 
 
 CREATE TABLE conteudos (
-	id_conteudos INT IDENTITY(1,1) NOT NULL,
+	id INT IDENTITY(1,1) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	classificacao VARCHAR(100) NOT NULL,
-	id_prof INT NOT NULL
-	PRIMARY KEY(id_conteudos)
-	FOREIGN KEY (id_prof) REFERENCES professor(id_prof)
+	id_prof INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (id_prof) REFERENCES professor(id)
 	); 
 CREATE TABLE arquivos (
-	id_arquivos INT IDENTITY(1,1),
+	id INT IDENTITY(1,1),
 	nome VARCHAR(100),
-	id_conteudos INT
-	PRIMARY KEY (id_arquivos)
+	id_conteudos INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_conteudos) REFERENCES conteudos (id)
 	);	
 CREATE TABLE comentario (
-	id_comentarios INT IDENTITY(1,1) NOT NULL,
+	id INT IDENTITY(1,1) NOT NULL,
 	texto VARCHAR(1000) NOT NULL,
-	id_usuario INT NOT NULL
-	PRIMARY KEY (id_comentarios)
+	id_usuario INT NOT NULL,
+	id_conteudos INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_usuario) REFERENCES usuario (id),
+	FOREIGN KEY (id_conteudos) REFERENCES conteudos (id)
 	);
 
 
