161d065244ddeed9467a221699c53c5cf7dff2be
Gabriel Barbosa <gabriel.barbosa3009@outlook.com>
2018-09-12 10:04:57 -0300

    adicionando código

---
+++ Código/cadastro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+
+<style media="screen">
+  body, html {
+    height: 100%;
+  }
+  body {
+    height: 100%;
+    background: #3846DC;
+    background: -webkit-linear-gradient(#3846DC, #00EAAC);
+    background: -o-linear-gradient(#3846DC, #00EAAC);
+    background: -moz-linear-gradient(#3846DC, #00EAAC);
+    background: linear-gradient(#3846DC, #00EAAC);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  div{
+    background-color: white;
+    border-radius: 10px;
+    font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+    text-align: center;
+    margin-left: 600px;
+    margin-right: 600px;
+    margin-top: 200px;
+  }
+</style>
+<html lang="pt-br">
+    <head>
+          <title>Cadastro</title>
+    <head>
+        <body>
+        <!--<h1>Crie sua conta</h1>-->
+        <div> <br><br>
+          <form action="falta colocaaaaaaaaaaaaaaaaaaaaaaaaaar" method="post">
+
+
+          <label><b>CADASTRO</b></label><br><br>
+          <label>Nome: </label><input type="name" style="border:0;" name="nome" required placeholder="Digite seu nome."><br>
+          <label>E-mail: </label><input type="email" style="border:0;" name="email" required placeholder="Digite seu e-mail."><br>
+          <label>Senha: </label><input type="password" style="border:0;" required name="senha"><br><br>
+          <input type="button" value="Enviar" name="botao"><br><br>
+
+          </form>
+        </div>
+        </body>
+</html>
